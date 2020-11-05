964f0e6f311f48817a912fcd8b8b3aa43cdc49d4
Eugene Birondo <jbirondo@gmail.com>
2020-11-04 19:00:41 -0800

    Making splash page

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,7 +1,27 @@
 import React from "react"
 
 class Splash extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            matchUps: [],
+        }
+    }
 
+    componentDidMount() {
+        fetch
+    }
+
+    fetchMatchUps(){
+        
+    }
+
+
+    render(){
+        return(
+            <div>"Hello World</div>
+        )
+    }
 }
 
 export default Splash
