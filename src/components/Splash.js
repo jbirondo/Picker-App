e3f33c643887e9d693f13012e334880a83598c3f
Eugene Birondo <jbirondo@gmail.com>
2020-11-04 19:14:51 -0800

    Fetching from API

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -5,19 +5,43 @@ class Splash extends React.Component{
         super()
         this.state = {
             matchUps: [],
+            fetchErr: null,
+            isLoading: true
         }
     }
 
     componentDidMount() {
-        fetch
+        this.fetchMatchUps()
     }
 
     fetchMatchUps(){
-        
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        headers.append('Origin', 'http://localhost:3000');
+        const url = "http://flask-env.eba-uqj6sxyv.us-west-1.elasticbeanstalk.com/"
+        fetch(url, {
+            mode: 'cors',
+            credentials: 'include',
+            method: 'GET',
+            headers: headers
+        }).then(response => {
+            response.json().then(response => {
+                this.setState({
+                    matchUps: response.MatchUps
+                })
+            })
+        }).catch(error => {
+            this.setState({
+                fetchErr: error,
+                isLoading: false
+            })
+        })
     }
 
 
     render(){
+        console.table(this.state.matchUps)
         return(
             <div>"Hello World</div>
         )
